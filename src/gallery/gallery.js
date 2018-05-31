29a77359a9bdc4c0b28eab5402140c0cb10c383c
SethDonohue <donohue.seth@gmail.com>
2018-05-31 14:45:57 -0700

    added if logic to map to only show one pic depending on slideCount state

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -4,20 +4,48 @@ import './gallery.css';
 // TODO: make gallery a sliding gallery!
 
 class Gallery extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      slideCount: 0,
+    }
+
+    this.previousImg = this.previousImg.bind(this);
+    this.nextImg = this.nextImg.bind(this);
+  }
+
+  previousImg() {
+    this.setState({
+      slideCount: this.state.slideCount - 1,
+    })
+  }
+
+  nextImg() {
+    this.setState({
+      slideCount: this.state.slideCount + 1,
+    })
+  }
+
+
   render() {
     const { photos } = this.props;
 
     return (
       <div className="photo-gallery">
       {
-        photos.map((photo, key) => {
-          return (
-            <div key={photo.id}>
-              <img src={photo.images.standard_resolution.url} alt={photo.caption} />
-            </div>
-          )
+        photos.map((photo, index) => {
+          if(this.state.slideCount === index) {
+            console.log('Current Photo', photo, index);
+            return (
+              <div key={photo.id}>
+                <img src={photo.images.standard_resolution.url} alt={photo.caption} />
+                <span>{photo.caption ? photo.caption.text : ''}</span>
+              </div>
+            )
+          }
+          return '';
         })
-      }
+      } 
       </div>
     )
   }
