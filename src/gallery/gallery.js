3cf349b1afd894e9599ba128767cd47a12afa04a
SethDonohue <donohue.seth@gmail.com>
2018-05-31 17:09:53 -0700

    styled for flex

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -26,25 +26,34 @@ class Gallery extends Component {
 
 
   render() {
+    console.log(this.state.slideCount);
     const { photos } = this.props;
+    
+    const previousJSX = this.state.slideCount > 0 ? (
+      <button id="button-previous" onClick={this.previousImg}> Previous </button>
+    ): null;
+
+    const nextJSX = this.state.slideCount < 32 ? (
+      <button id="button-next" onClick={this.nextImg}> Next </button>
+    ): null;
 
     return (
       <div className="photo-gallery">
-      <button onClick={this.previousImg}> Previous </button>
-      <button onClick={this.nextImg}> Next </button>
-      {
-        photos.map((photo, index) => {
+      {previousJSX}
+      {nextJSX}
+      <div id="img-container">
+        {photos.map((photo, index) => {
           if(this.state.slideCount === index) {
             return (
               <div key={photo.id}>
                 <img src={photo.images.standard_resolution.url} alt={photo.caption} />
-                <span>{photo.caption ? photo.caption.text : ''}</span>
+                <p>{photo.caption ? photo.caption.text : ''}</p>
               </div>
             )
           }
           return '';
-        })
-      } 
+        })}
+        </div>
       </div>
     )
   }
